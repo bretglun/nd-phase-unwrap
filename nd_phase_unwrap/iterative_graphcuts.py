9c1fb784dd32d186bedf0d3b4ed35e7727a8e106
Johan Berglund <berglund.hpj@gmail.com>
2026-05-21 12:12:22 +0200

    IGC: Improve variable names

--- nd_phase_unwrap/iterative_graphcuts.py
+++ nd_phase_unwrap/iterative_graphcuts.py
@@ -36,19 +36,19 @@ def get_neighbour_indices(neighbourhood, shape, cyclic):
     return ngb_indices.reshape(num_ngb, num_voxels), edge_ngb.reshape(num_ngb, num_voxels)
 
 
-def solve_maxflow(D, V, ngb_indices):
-    num_nodes = V.shape[2]
+def solve_maxflow(unary_costs, binary_costs, ngb_indices):
+    num_nodes = binary_costs.shape[2]
     num_neighbours = len(ngb_indices)
     
     graph = maxflow.GraphFloat()
     graph.add_grid_nodes(num_nodes)
 
-    src_cap, snk_cap = (D[1], D[0]) if D is not None else (np.zeros(num_nodes, dtype=V.dtype), np.zeros(num_nodes, dtype=V.dtype))
+    src_cap, snk_cap = (unary_costs[1], unary_costs[0]) if unary_costs is not None else (np.zeros(num_nodes, dtype=binary_costs.dtype), np.zeros(num_nodes, dtype=binary_costs.dtype))
     
     ii = np.arange(num_nodes)
     for q in range(num_neighbours): # loop over neighbours
         jj = ngb_indices[q]
-        Ai, Bi, Ci = V[0, q, ii], V[1, q, ii], V[2, q, ii]
+        Ai, Bi, Ci = binary_costs[0, q, ii], binary_costs[1, q, ii], binary_costs[2, q, ii]
         b, c = Bi - Ai, Ci - Ai  # b+c >= 0 (submodular)
         # Reparameterise into non-negative tedges + directed edges.
         src_cap[ii] += np.maximum(0.0, -b)
@@ -66,38 +66,38 @@ def solve_maxflow(D, V, ngb_indices):
     return graph.get_grid_segments(ii)
 
 
-def beta_jump_move(phase, beta, wD, wV, ngb_indices):
+def beta_jump_move(phase, beta, unary_weights, binary_weights, ngb_indices):
     p = phase.flatten()
     pb = p + beta
-    D = np.array((wD * p**2, wD * pb**2)) if wD is not None else None
-    V = wV * [
+    unary_costs = np.array((unary_weights * p**2, unary_weights * pb**2)) if unary_weights is not None else None
+    binary_costs = binary_weights * [
         (p  -  p[ngb_indices])**2,
         (p  - pb[ngb_indices])**2,
         (pb -  p[ngb_indices])**2,
         (pb - pb[ngb_indices])**2]
-    label = solve_maxflow(D, V, ngb_indices)
+    label = solve_maxflow(unary_costs, binary_costs, ngb_indices)
     p[label==1] += beta
     return p.reshape(phase.shape)
 
 
-def get_energy(phase, wD, wV, ngb_indices):
+def get_energy(phase, unary_weights, binary_weights, ngb_indices):
     p = phase.flatten()
-    energy = np.sum(wD * p**2) if wD is not None else 0.
-    energy += np.sum(wV * (p  - p[ngb_indices])**2)
+    energy = np.sum(unary_weights * p**2) if unary_weights is not None else 0.
+    energy += np.sum(binary_weights * (p  - p[ngb_indices])**2)
     return energy
 
 
 def get_weights(magn, pixel_spacing, neighbourhood, ngb_indices, edge_ngb, data_cost=0):
     M2p = magn.flatten()**2
-    wD = data_cost * M2p if data_cost > 0 else None
-    wV = np.zeros((magn.ndim, *magn.shape))
+    unary_weights = data_cost * M2p if data_cost > 0 else None
+    binary_weights = np.zeros((magn.ndim, *magn.shape))
     with np.errstate(divide='ignore', invalid='ignore'): # suppress divide-by-zero warning
-        wV = 1 / (1/M2p + 1/M2p[ngb_indices])
-    wV[np.isnan(wV)] = 0.
+        binary_weights = 1 / (1/M2p + 1/M2p[ngb_indices])
+    binary_weights[np.isnan(binary_weights)] = 0.
     distance = np.linalg.norm(neighbourhood * pixel_spacing, axis=1)[:, None]
-    wV /= distance * np.sum(1 / distance)
-    wV[edge_ngb] = 0
-    return wD, wV
+    binary_weights /= distance * np.sum(1 / distance)
+    binary_weights[edge_ngb] = 0
+    return unary_weights, binary_weights
 
 
 def remove_drift(phase, magn, period):
@@ -112,15 +112,15 @@ def unwrap(arr, config):
     neighbourhood = get_neighbourhood(config.neighbourhood_radius, config.pixel_spacing)
     ngb_indices, edge_ngb = get_neighbour_indices(neighbourhood, arr.shape, config.cyclic)
 
-    wD, wV = get_weights(magn, config.pixel_spacing, neighbourhood, ngb_indices, edge_ngb, config.data_cost)
+    unary_weights, binary_weights = get_weights(magn, config.pixel_spacing, neighbourhood, ngb_indices, edge_ngb, config.data_cost)
 
-    min_energy = get_energy(phase, wD, wV, ngb_indices)
+    min_energy = get_energy(phase, unary_weights, binary_weights, ngb_indices)
     improved = True
     while improved:
         improved = False
         for beta in [2 * np.pi, -2 * np.pi]: # 2pi jump moves with alternating sign
-            phase_updated = beta_jump_move(phase, beta, wD, wV, ngb_indices)
-            energy = get_energy(phase_updated, wD, wV, ngb_indices)
+            phase_updated = beta_jump_move(phase, beta, unary_weights, binary_weights, ngb_indices)
+            energy = get_energy(phase_updated, unary_weights, binary_weights, ngb_indices)
             if energy < min_energy:
                 improved = True
                 phase = phase_updated
